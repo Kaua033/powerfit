a08b4a7fc1577860c457447c9f83e7fe478e90a5
Kaua033 <suelen.kaua16@gmail.com>
2025-12-17 13:03:11 -0300

    feat:adicionado variaveis de ambiente na aplicação

--- src/main/java/PowerFit/API/TREINADORES/TreinadoresController.java
+++ src/main/java/PowerFit/API/TREINADORES/TreinadoresController.java
@@ -1,4 +1,18 @@
 package PowerFit.API.TREINADORES;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("TREINADORES")
 public class TreinadoresController {
+
+private treinadoresService treinadoresService;
+
+    public TreinadoresController(treinadoresService treinadoresService) {
+        this.treinadoresService = treinadoresService;
+    }
+
+
+
 }
