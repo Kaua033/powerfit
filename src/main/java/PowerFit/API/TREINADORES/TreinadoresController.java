09f88c8f43e75d178631e3b62329dcabb2fd907e
Kaua033 <suelen.kaua16@gmail.com>
2025-12-19 01:29:50 -0300

    feat:Adicionado metodos CRUD na classe TREINADORES

--- src/main/java/PowerFit/API/TREINADORES/TreinadoresController.java
+++ src/main/java/PowerFit/API/TREINADORES/TreinadoresController.java
@@ -1,9 +1,6 @@
 package PowerFit.API.TREINADORES;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.plaf.PanelUI;
 import java.util.List;
@@ -17,19 +14,40 @@ public class TreinadoresController {
     public TreinadoresController(treinadoresService treinadoresService) {
         this.treinadoresService = treinadoresService;
     }
-//localhost:8080/TREINADORES/listarT
-@GetMapping("listarT")
+
+    //localhost:8080/TREINADORES/listarT
+    @GetMapping("listarT")
     public List<TreinadoresModel> listarT() {
         return treinadoresService.listar();
 
     }
-@GetMapping("ID/{ID}")
-    public TreinadoresModel listarID(@PathVariable Long ID){
- TreinadoresModel TID = treinadoresService.treinadoresModel(ID);
- if (treinadoresService.listar() != null){
-     return treinadoresService.treinadoresModel(ID);
- }
- return null;
+    //localhost:8080/TREINADORES/ID/?
+    @GetMapping("ID/{ID}")
+    public TreinadoresModel listarID(@PathVariable Long ID) {
+        TreinadoresModel TID = treinadoresService.listarID(ID);
+        if (treinadoresService.listar() != null) {
+            return treinadoresService.listarID(ID);
+        }
+        return null;
+    }
+    //localhost:8080/TREINADORES/DELETAR/ID
+    @DeleteMapping("DELETAR/{ID}")
+    public void Deletar(@PathVariable Long ID){
+        treinadoresService.DELETAR(ID);
+    }
+    //localhost:8080/TREINADORES/CRIAR
+   @PostMapping("CRIAR")
+   public TreinadoresModel Post( @RequestBody TreinadoresModel CRIAR){
+        return treinadoresService.CRiAR(CRIAR);
+   }
+    //localhost:8080/TREINADORES/ATUALIZAR
+    @PutMapping("ATUALIZAR")
+   public  TreinadoresModel ATUALIZAR(@PathVariable Long ID, @RequestBody TreinadoresModel TREINADORESVCT){
+        return treinadoresService.ATUALIZAR(ID,TREINADORESVCT);
 }
+
+
+
+
 
 }
--- src/main/java/PowerFit/API/TREINADORES/treinadoresService.java
+++ src/main/java/PowerFit/API/TREINADORES/treinadoresService.java
@@ -1,6 +1,8 @@
 package PowerFit.API.TREINADORES;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,9 +22,26 @@ public class treinadoresService {
 }
 
 
- public TreinadoresModel treinadoresModel(Long ID){
+ public TreinadoresModel listarID(Long ID){
      Optional<TreinadoresModel> treinadoresModel = treinadoresRepository.findById(ID);
      return treinadoresModel.orElse(null);
  }
 
+ public void DELETAR(Long ID){
+        treinadoresRepository.deleteById(ID);
+ }
+
+public TreinadoresModel CRiAR( TreinadoresModel CRIARMODEL){
+        return treinadoresRepository.save(CRIARMODEL);
+}
+
+
+public  TreinadoresModel ATUALIZAR(Long ID,TreinadoresModel ATUALIZARVC){
+        // treinadoresRepository.save(Long ID, TreinadoresModel ATUALIZARVC);
+    if (treinadoresRepository.existsById(ID)) {
+        ATUALIZARVC.setID(ID);
+    return treinadoresRepository.save(ATUALIZARVC);
+    }
+return  null;
+    }
 }
